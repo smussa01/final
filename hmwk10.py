12a61048b4cfe84e78402a6850c66d48a3cb21e1
smussa01 <45742811+smussa01@users.noreply.github.com>
2018-12-09 16:28:30 -0600

    Add files via upload

---
+++ hmwk10.py
@@ -0,0 +1,185 @@
+import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
+
+# Make an API call, and store the response.
+url = 'https://api.github.com/search/repositories?q=language:javascript&sort=stars'
+r = requests.get(url)
+print("Status code:", r.status_code)
+
+# Store API response in a variable.
+response_dict = r.json()
+print("Total repositories:", response_dict['total_count'])
+
+# Explore information about the repositories.
+repo_dicts = response_dict['items']
+
+names, plot_dicts = [], []
+for repo_dict in repo_dicts:
+    names.append(repo_dict['name'])
+
+    # When a project is removed, it's still listed with stars.
+    #   So it's in the top projects, but has no description. The description
+    #   is None, which causes an exception when being used as a label.
+    if repo_dict['description']:
+        desc = repo_dict['description']
+    else:
+        desc = 'No description provided.'
+
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],
+        'label': desc,
+        'xlink': repo_dict['html_url'],
+    }
+    plot_dicts.append(plot_dict)
+
+# Make visualization.
+my_style = LS('#333366', base_style=LCS)
+my_style.title_font_size = 24
+my_style.label_font_size = 14
+my_style.major_label_font_size = 18
+
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+
+chart = pygal.Bar(my_config, style=my_style)
+chart.title = 'Most-Starred JavaScript Projects on GitHub'
+chart.x_labels = names
+
+chart.add('', plot_dicts)
+chart.render_to_file('js_repos.svg')
+
+import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
+
+from operator import itemgetter
+
+# Make an API call, and store the response.
+url = 'https://hacker-news.firebaseio.com/v0/topstories.json'
+r = requests.get(url)
+print("Status code:", r.status_code)
+
+# Process information about each submission.
+submission_ids = r.json()
+submission_dicts = []
+for submission_id in submission_ids[:30]:
+    # Make a separate API call for each submission.
+    url = ('https://hacker-news.firebaseio.com/v0/item/' +
+           str(submission_id) + '.json')
+    submission_r = requests.get(url)
+    print(submission_r.status_code)
+    response_dict = submission_r.json()
+
+    submission_dict = {
+        'title': response_dict['title'],
+        'link': 'http://news.ycombinator.com/item?id=' + str(submission_id),
+        'comments': response_dict.get('descendants', 0)
+    }
+    submission_dicts.append(submission_dict)
+
+submission_dicts = sorted(submission_dicts, key=itemgetter('comments'),
+                          reverse=True)
+
+for submission_dict in submission_dicts:
+    print("\nTitle:", submission_dict['title'])
+    print("Discussion link:", submission_dict['link'])
+    print("Comments:", submission_dict['comments'])
+
+titles, plot_dicts = [], []
+for submission_dict in submission_dicts:
+    titles.append(submission_dict['title'])
+    plot_dict = {
+        'value': submission_dict['comments'],
+        'label': submission_dict['title'],
+        'xlink': submission_dict['link'],
+    }
+    plot_dicts.append(plot_dict)
+
+# Make visualization.
+my_style = LS('#333366', base_style=LCS)
+my_style.title_font_size = 24
+my_style.label_font_size = 14
+my_style.major_label_font_size = 18
+
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+my_config.y_title = 'Number of Comments'
+
+chart = pygal.Bar(my_config, style=my_style)
+chart.title = 'Most Active Discussions on Hacker News'
+chart.x_labels = titles
+
+chart.add('', plot_dicts)
+chart.render_to_file('hn_discussions.svg')
+
+import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
+
+def get_response():
+    """Make an api call, and return the response."""
+    url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+    r = requests.get(url)
+    return r
+
+def get_repo_dicts(response):
+    """Return a set of dicts representing the most popular repositories."""
+    response_dict = r.json()
+    repo_dicts = response_dict['items']
+    return repo_dicts
+
+def get_names_plot_dicts(repo_dicts):
+    """Process the set of repository dicts, and pull out data for plotting."""
+    names, plot_dicts = [], []
+    for repo_dict in repo_dicts:
+        names.append(repo_dict['name'])
+
+        # Some projects lack a description, which causes an error when
+        #  labeling bars. Specify a label if there's no description.
+        description = repo_dict['description']
+        if not description:
+            description = "No description provided."
+
+        plot_dict = {
+            'value': repo_dict['stargazers_count'],
+            'label': description,
+            'xlink': repo_dict['html_url'],
+            }
+        plot_dicts.append(plot_dict)
+    return names, plot_dicts
+
+def make_visualization(names, plot_dicts):
+    """Make visualization of most popular repositories."""
+    my_style = LS('#333366', base_style=LCS)
+    my_style.title_font_size = 24
+    my_style.label_font_size = 14
+    my_style.major_label_font_size = 18
+
+    my_config = pygal.Config()
+    my_config.x_label_rotation = 45
+    my_config.show_legend = False
+    my_config.truncate_label = 15
+    my_config.show_y_guides = False
+    my_config.width = 1000
+
+    chart = pygal.Bar(my_config, style=my_style)
+    chart.title = 'Most-Starred Python Projects on GitHub'
+    chart.x_labels = names
+
+    chart.add('', plot_dicts)
+    chart.render_to_file('python_repos.svg')
+
+
+r = get_response()
+repo_dicts = get_repo_dicts(r)
+names, plot_dicts = get_names_plot_dicts(repo_dicts)
+make_visualization(names, plot_dicts)
---
+++ hmwk9.py
@@ -0,0 +1,244 @@
+import csv
+from datetime import datetime
+
+from matplotlib import pyplot as plt
+
+# Get dates, high, and low temperatures from file.
+filename = 'sitka_weather_2014.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    dates, highs, lows = [], [], []
+    for row in reader:
+        try:
+            current_date = datetime.strptime(row[0], "%Y-%m-%d")
+            high = int(row[1])
+            low = int(row[3])
+        except ValueError:
+            print(current_date, 'missing data')
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
+
+# Plot data.
+fig = plt.figure(dpi=128, figsize=(10, 6))
+plt.plot(dates, highs, c='red', alpha=0.5)
+plt.plot(dates, lows, c='blue', alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+
+# Format plot.
+title = "Daily high and low temperatures - 2014\nSitka, AK"
+plt.title(title, fontsize=20)
+plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
+plt.ylabel("Temperature (F)", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+plt.ylim(10, 120)
+
+plt.show()
+
+import csv
+from datetime import datetime
+
+from matplotlib import pyplot as plt
+
+def get_weather_data(filename, dates, highs, lows):
+    """Get the highs and lows from a data file."""
+    with open(filename) as f:
+        reader = csv.reader(f)
+        header_row = next(reader)
+
+        # dates, highs, lows = [], [], []
+        for row in reader:
+            try:
+                current_date = datetime.strptime(row[0], "%Y-%m-%d")
+                high = int(row[1])
+                low = int(row[3])
+            except ValueError:
+                print(current_date, 'missing data')
+            else:
+                dates.append(current_date)
+                highs.append(high)
+                lows.append(low)
+
+# Get weather data for Sitka.
+dates, highs, lows = [], [], []
+get_weather_data('sitka_weather_2014.csv', dates, highs, lows)
+
+# Plot Sitka weather data.
+fig = plt.figure(dpi=128, figsize=(10, 6))
+plt.plot(dates, highs, c='red', alpha=0.6)
+plt.plot(dates, lows, c='blue', alpha=0.6)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.15)
+
+# Get Death Valley data.
+dates, highs, lows = [], [], []
+get_weather_data('death_valley_2014.csv', dates, highs, lows)
+
+# Add Death Valley data to current plot.
+plt.plot(dates, highs, c='red', alpha=0.3)
+plt.plot(dates, lows, c='blue', alpha=0.3)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.05)
+
+# Format plot.
+title = "Daily high and low temperatures - 2014"
+title += "\nSitka, AK and Death Valley, CA"
+plt.title(title, fontsize=20)
+plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
+plt.ylabel("Temperature (F)", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+plt.ylim(10, 120)
+
+plt.show()
+
+import csv
+from datetime import datetime
+
+from matplotlib import pyplot as plt
+
+# Get dates and rainfall data from data file.
+#  Rainfall data is in column 19.
+filename = 'sitka_rainfall_2015.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    dates, rainfalls = [], []
+    for row in reader:
+        try:
+            current_date = datetime.strptime(row[0], "%Y-%m-%d")
+            rainfall = float(row[19])
+        except ValueError:
+            print(current_date, 'missing data')
+        else:
+            dates.append(current_date)
+            rainfalls.append(rainfall)
+
+# Plot data.
+fig = plt.figure(dpi=128, figsize=(10, 6))
+plt.plot(dates, rainfalls, c='blue', alpha=0.5)
+plt.fill_between(dates, rainfalls, facecolor='blue', alpha=0.2)
+
+# Format plot.
+title = "Daily rainfall amounts - 2015\nSitka, AK"
+plt.title(title, fontsize=20)
+plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
+plt.ylabel("Rainfall (in)", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
+
+import csv
+from datetime import datetime
+
+from matplotlib import pyplot as plt
+
+# Get dates and rainfall data from data file.
+#  Rainfall data is in column 19.
+filename = 'sitka_rainfall_2015.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    dates, rainfalls, totals = [], [], []
+    for row in reader:
+        try:
+            current_date = datetime.strptime(row[0], "%Y-%m-%d")
+            rainfall = float(row[19])
+        except ValueError:
+            print(current_date, 'missing data')
+        else:
+            dates.append(current_date)
+            rainfalls.append(rainfall)
+            if totals:
+                totals.append(totals[-1] + rainfall)
+            else:
+                totals.append(rainfall)
+
+# Plot data.
+fig = plt.figure(dpi=128, figsize=(10, 6))
+plt.plot(dates, rainfalls, c='blue', alpha=0.5)
+plt.fill_between(dates, rainfalls, facecolor='blue', alpha=0.2)
+
+plt.plot(dates, totals, c='blue', alpha=0.75)
+plt.fill_between(dates, totals, facecolor='blue', alpha=0.05)
+
+# Format plot.
+title = "Daily rainfall amounts and cumulative rainfall - 2015\nSitka, AK"
+plt.title(title, fontsize=20)
+plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
+plt.ylabel("Rainfall (in)", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
+
+import json
+
+import pygal
+from pygal.style import LightColorizedStyle as LCS, RotateStyle as RS
+from pygal.maps.world import World
+
+from country_codes import get_country_code
+
+# Load the data into a list.
+filename = 'global_gdp.json'
+with open(filename) as f:
+    gdp_data = json.load(f)
+
+# Build a dictionary of gdp data.
+cc_gdps = {}
+for gdp_dict in gdp_data:
+    if gdp_dict['Year'] == '2014':
+        country_name = gdp_dict['Country Name']
+        gdp = int(float(gdp_dict['Value']))
+        code = get_country_code(country_name)
+        if code:
+            cc_gdps[code] = gdp
+
+# Group the countries into 3 gdp levels.
+#  Less than 5 billion, less than 50 billion, >= 50 billion.
+#  Also, convert to billions for displaying values.
+cc_gdps_1, cc_gdps_2, cc_gdps_3 = {}, {}, {}
+for cc, gdp in cc_gdps.items():
+    if gdp < 5000000000:
+        cc_gdps_1[cc] = round(gdp / 1000000000)
+    elif gdp < 50000000000:
+        cc_gdps_2[cc] = round(gdp / 1000000000)
+    else:
+        cc_gdps_3[cc] = round(gdp / 1000000000)
+
+# See how many countries are in each level.
+print(len(cc_gdps_1), len(cc_gdps_2), len(cc_gdps_3))
+
+wm_style = RS('#336699', base_style=LCS)
+wm = World(style=wm_style)
+wm.title = 'Global GDP in 2014, by Country (in billions USD)'
+wm.add('0-5bn', cc_gdps_1)
+wm.add('5bn-50bn', cc_gdps_2)
+wm.add('>50bn', cc_gdps_3)
+
+wm.render_to_file('global_gdp.svg')
+
+import unittest
+
+from country_codes import get_country_code
+
+class CountryCodesTestCase(unittest.TestCase):
+    """Tests for country_codes.py."""
+
+    def test_get_country_code(self):
+        country_code = get_country_code('Andorra')
+        self.assertEqual(country_code, 'ad')
+
+        country_code = get_country_code('United Arab Emirates')
+        self.assertEqual(country_code, 'ae')
+
+        country_code = get_country_code('Afghanistan')
+        self.assertEqual(country_code, 'af')
+
+unittest.main()
